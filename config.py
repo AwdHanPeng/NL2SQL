02eacc6995ab0962ea823f2120380a68bee40a65
qikahh <46292821+qikahh@users.noreply.github.com>
2020-10-19 22:42:49 +0800

    修改数据处理
    截断机制待设计

---
+++ config.py
@@ -0,0 +1,39 @@
+import warnings
+
+
+class DefaultConfig(object):
+    seed = 3435
+    num_epochs = 100
+    use_gpu = True  # user GPU or not
+    gpu_id = 0
+    use_max_length = True
+    max_length = {
+        'sql': 65,
+        'utter': 25,
+        'db': 200,
+        'turn': 7
+    }
+
+
+def parse(self, kwargs):
+        '''
+        user can update the default hyperparamter
+        '''
+        # 更新配置参数
+        for k, v in kwargs.iteritems():
+            if not hasattr(self, k):
+                # 警告还是报错，取决于你个人的喜好
+                warnings.warn("Warning: opt has not attribut %s" % k)
+            setattr(self, k, v)
+
+        print('*************************************************')
+        print('user config:')
+        for k, v in self.__class__.__dict__.items():
+            if not k.startswith('__'):
+                print("{} => {}".format(k, getattr(self, k)))
+
+        print('*************************************************')
+
+
+DefaultConfig.parse = parse
+opt = DefaultConfig()
--- dataset/dataset.py
+++ dataset/dataset.py
@@ -5,9 +5,13 @@ import re
 from torch.utils.data import Dataset
 import matplotlib.pyplot as plt
 from collections import Counter
+from config import opt
 
 
 # 将数据改为全序列形式并添加<PAD>, 单个数据形式 [columns,u1,s1,u2,s2...u_m,s_m]
+'''
+截断机制未设计好
+'''
 class DataLoad(Dataset):
     def __init__(self, max_length, data_ori):
         self.max_length = max_length
@@ -23,11 +27,15 @@ class DataLoad(Dataset):
         _temporal = _type
         _modality = _type
         for index in range(len(database['tokens'])):
+            if index >= self.max_length['db']:
+                break
             _data[index] = database['tokens'][index]
             _type[index] = database['db_signal'][index]
             _modality[index] = database['modality_signal'][index]
             _temporal[index] = database['temporal_signal'][index]
         for turn in range(len(pair)):
+            if turn >= self.max_length['turn']:
+                break
             for index in range(len(pair[turn]['utter']['content'])):
                 _data[index + turn * (self.max_length['utter'] + self.max_length['sql']) + self.max_length['db']] \
                     = pair[turn]['utter']['content'][index]
@@ -47,7 +55,8 @@ class DataLoad(Dataset):
                     = pair[turn]['sql']['temporal_signal'][index]
                 _modality[index + turn * (self.max_length['utter'] + self.max_length['sql']) + self.max_length['db']] \
                     = pair[turn]['sql']['modality_signal'][index]
-        return {'content':_data, 'db_signal': _type, 'temporal_signal': _temporal, 'modality_signal': _modality}
+        _data[0] = '[CLS]'
+        return {'content': _data, 'db_signal': _type, 'temporal_signal': _temporal, 'modality_signal': _modality}
 
     def __getitem__(self, index):
         assert index < len(self.data)
@@ -74,8 +83,8 @@ class DataSetLoad():
         self.max_length = self.get_length()
 
         # 如果自定义最大长度则覆盖
-        if opt['use_max_length']:
-            self.max_length = opt['max_length']
+        if opt.use_max_length:
+            self.max_length = opt.max_length
 
         # 生成数据DataSet
         self.train = DataLoad(self.max_length, self.train_ori)
@@ -99,23 +108,25 @@ class DataSetLoad():
             table_num = 0
             for column in table_schema['column_names']:
                 if column[0] >= table_num:
+                    # 加入当前一系列column对应的table
                     split_table = re.split('[ _]', table_schema['table_names'][table_num])
+                    table_num += 1
                     split_database['tokens'] += ['[SEP]']
                     split_database['tokens'] += split_table
-                    split_database['db_signal'] += [0]
-                    split_database['db_signal'] += [1 for i in split_table]
-                    split_database['modality_signal'] += [0]
+                    split_database['db_signal'] += [table_num]
+                    split_database['db_signal'] += [table_num for i in split_table]
+                    split_database['modality_signal'] += [1]
                     split_database['modality_signal'] += [1 for i in split_table]
                     split_database['temporal_signal'] += [0]
                     split_database['temporal_signal'] += [0 for i in split_table]
-                    table_num += 1
+                # 处理column
                 split_column = re.split('[ _]', column[1])
                 split_database['tokens'] += ['[SEP]']
                 split_database['tokens'] += split_column
-                split_database['db_signal'] += [0]
-                split_database['db_signal'] += [2 for i in split_column]
-                split_database['modality_signal'] += [0]
-                split_database['modality_signal'] += [1 for i in split_column]
+                split_database['db_signal'] += [table_num]
+                split_database['db_signal'] += [table_num for i in split_column]
+                split_database['modality_signal'] += [2]
+                split_database['modality_signal'] += [2 for i in split_column]
                 split_database['temporal_signal'] += [0]
                 split_database['temporal_signal'] += [0 for i in split_column]
             table_schema['split_database'] = split_database
@@ -145,39 +156,47 @@ class DataSetLoad():
         return database_schema, column_names_surface_form, column_names_embedder_input
 
     # 填充数据类型序列
-    # 返回字典{'utter':{'tokens','db', 'temporal', 'modality'},'sql':{'tokens','db', 'temporal', 'modality'}}
+    # 返回字典{'utter':{'tokens','modality', 'temporal', 'db'},'sql':{'tokens', 'modality', 'temporal', 'db'}}
     def get_pair_type(self, turn, sql, utter, database):
         database_ori = self.database_schema[database['id']]
         # 计算sql
         stat = 0
         word = ''
-        db_type = []
+        modality_type = []
         for item in sql:
+            # 当前词未出现在db中，则表示之前一个词组已经结束，且当前词为一个关键字
             if item not in database['tokens']:
                 if stat:
+                    # 词组为一个表名
                     if word in database_ori['table_names']:
-                        db_type += [1 for _ in range(stat)]
+                        modality_type += [1 for _ in range(stat)]
+                    # 词组为一个列名
                     else:
-                        db_type += [2 for _ in range(stat)]
+                        modality_type += [2 for _ in range(stat)]
+                # 处理完一个词组，初始化，将当前关键字类型加入
                 word = ''
                 stat = 0
-                db_type.append(3)
+                modality_type.append(3)
+            # 将当前词加入词表
             else:
                 stat += 1
                 word += item
+        # 若结尾词组非空，记为当前词组
         if stat:
-            if word in database_ori:
-                db_type += [1 for _ in range(stat)]
+            # 词组为一个表名
+            if word in database_ori['table_names']:
+                modality_type += [1 for _ in range(stat)]
+            # 词组为一个列名
             else:
-                db_type += [2 for _ in range(stat)]
+                modality_type += [2 for _ in range(stat)]
         temporal_type = [turn for _ in sql]
-        modality_type = [3 for _ in sql]
+        db_type = [0 for _ in sql]
+        sql = {'content': ['[SEP]']+sql+['[SEP]'], 'modality_signal': [0]+modality_type+[0], 'temporal_signal': [turn]+temporal_type+[turn], 'db_signal': [0]+db_type+[0]}
         # 计算utter
-        sql = {'content': ['[SEP]']+sql, 'db_signal': [0]+db_type, 'modality_signal': [0]+modality_type, 'temporal_signal': [0]+temporal_type}
-        db_type = [0 for _ in utter]
         temporal_type = [turn for _ in utter]
-        modality_type = [2 for _ in utter]
-        utter = {'content': ['[SEP]']+utter, 'db_signal': [0]+db_type, 'modality_signal': [0]+modality_type, 'temporal_signal': [0]+temporal_type}
+        modality_type = [4 for _ in utter]
+        db_type = [0 for _ in utter]
+        utter = {'content': ['[SEP]']+utter+['[SEP]'], 'modality_signal': [0]+modality_type+[0], 'temporal_signal': [turn]+temporal_type+[turn], 'db_signal': [0]+db_type+[0]}
         return {'utter': utter, 'sql': sql}
 
     # 获取各类型最大长度，并生成分割标记好的序列化数据
@@ -259,7 +278,7 @@ class DataSetLoad():
             legth['utter'].append(len(utterance) + 1)
             legth['db'].append(len(item['split_database']['tokens']))
 
-        self.plt_length(legth)
+        # self.plt_length(legth)
         return max_legth
 
     # 统计长度
@@ -284,7 +303,7 @@ class DataSetLoad():
         plt.xlabel('length')
         plt.ylabel('num')
         plt.legend()
-        # plt.savefig(_type+'.png')
+        # plt.savefig('data_output/'+_type+'.png')
         plt.show()
         print(1)
 
@@ -299,7 +318,61 @@ class DataSetLoad():
 
 
 if __name__ == '__main__':
-    dataset = DataSetLoad({'use_max_length': False})
+    dataset = DataSetLoad(opt)
 
 
-# TODO：dataset的格式参考model的forward函数
+'''
+sample:
+{
+    "id": "",
+    "scenario": "",
+    "database_id": "hospital_1",
+    "interaction_id": 0,
+    "final": {
+        "utterance": "Find the department with the most employees.",
+        "sql": "SELECT name FROM department GROUP BY departmentID ORDER BY count(departmentID) DESC LIMIT 1;"
+    },
+    "interaction": [
+        {
+            "utterance": "What is the number of employees in each department ?",
+            "sql": "select count ( departmentid ) from department group by departmentid"
+        },
+        {
+            "utterance": "Which department has the most employees ? Give me the department name .",
+            "sql": "select name from department group by departmentid order by count ( departmentid ) desc limit value"
+        }
+    ]
+}
+__getitem__:
+{
+'Table':"<CLS> physician <SEP> department <SEP> affiliated with <SEP> procedures ....<SEP> undergoes <SEP> <PAD>...<PAD>“ <<<<<<<  小于args.tl的padding至args.tl，大于args.tl的样本需要进行截取
+'Column':"<SEP> * <SEP> employee id <SEP> name <SEP> position <SEP> ... <SEP> assisting nurse <SEP> <PAD>...<PAD>" <<<<<<<  小于args.cl的padding至args.cl，大于args.cl的样本需要进行截取
+‘Utterance’:
+['<SEP> What is the number of employees in each department ? <SEP> <PAD>...<PAD>','<SEP> Which department has the most employees ? Give me the department name . <SEP> <PAD>...<PAD>']
+<<<<<<<  utterance中每个句子的长度，如果小于args.ul padding到args.ul，如果大于args.ul那么就扔到最后的几个单词
+<<<<<<<  utterance的轮数我们不处理，原来有多少轮，这个list中就放多少轮
+'SQL':
+['<SEP> select count ( departmentid ) from department group by departmentid <EOS> <PAD>...<PAD> ',
+'<SEP> select name from department group by departmentid order by count ( departmentid ) desc limit value <EOS> <PAD>...<PAD> ']
+<<<<<<<  sql中每个句子的长度，如果小于args.sl就padding到args.sl，如果大于args.sl那么就扔到最后的几个单词
+<<<<<<<  sql的轮数我们不处理，原来有多少轮，这个list中就放多少轮
+'input': [
+concat
+] 
+<<<<<<<  将Table，Column，utterance1，sql1拼接起来 长度等于 tl+cl+ul+sl 的， 
+'Modality':
+[[1]* tl +[2]*cl + [4]*len(utterance1) + sql1的组合}
+,}
+] <<<<<<<<<1表示table 2表示column 3表示utterance 4表示sql 0无
+'position':
+不需要，在模型forward中即可定义
+’temporal‘：
+不需要，在模型forward中即可定义
+’dbcomponent‘:
+db 0 无  1 table1 2 table2 3 table3 先不考虑sql
+} 
+:param position: -
+:param modality: 0 无， 1 table 2 column 3 keyword 4 自然语言
+:param temporal: 0 db， 第一轮：1 ，，，
+:param db 0 无  1 table1 2 table2 3 table3 先不考虑sql
+'''
